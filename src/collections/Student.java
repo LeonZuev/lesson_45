f8d4c267234e883a47430c4d0836582d3552f3d8
Leon Zuev <loonatune@gmail.com>
2023-03-21 22:13:46 +0200

    Finish Student

---
+++ src/collections/Student.java
@@ -0,0 +1,85 @@
+package collections;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Student implements Comparable<Student> {
+  private String name;
+  private int score;
+
+  public Student(String name, int score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  public static Student reedStudent() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("name");
+    String name = br.readLine();
+    int score = Integer.parseInt(br.readLine());
+    return new Student(name, score);
+  }
+
+  public static Student readStudent() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Введите имя: ");
+    String name = br.readLine();
+    System.out.print("Введите рейтинговый балл: ");
+    int score = Integer.parseInt(br.readLine());
+
+    return new Student(name, score);
+  }
+
+  @Override
+  public String toString() {
+    return name + ": " + score;
+  }
+  // вариант 1 - сортировка по именам
+//  @Override
+//  public int compareTo(Student other) {
+//    // returns a negative integer, zero, or a positive integer as this object is less than,
+//    // equal to, or greater than the specified object.
+//
+//    // возвращает отрицательное целое число, ноль или положительное целое число,
+//    // поскольку этот объект меньше, равен или больше указанного объекта.
+//
+//    // если this < other - возвращаем любое отрицательное число (например, -1)
+//    // если this == other - возвращаем 0
+//    // если this > other - возвращаем любое положительное число (например, +1)
+//    // условие "x < y" равносильно условию "x.compareTo(y) < 0"
+////    if (/*this.*/name.compareTo(other.name) < 0) {
+////      // если имя this меньше (раньше по алфавиту), чем имя other, то и студент "меньше" -
+////      // должен быть раньше в списке
+////      return -1;
+////    }
+////    if (/*this.*/name.compareTo(other.name) > 0) {
+////      // если имя this больше (позже по алфавиту), чем имя other, то и студент "больше" -
+////      // должен быть позже в списке
+////      return 1;
+////    }
+////    // Если мы сюда попали, то имена совпадают. Можно вернуть 0
+////    return 0;
+//    return name.compareTo(other.name); // сравнить студентов - то же самое, что сравнить их имена
+//  }
+
+  // вариант 2 - сортировка по рейтингу
+  // если рейтинг одинаковый, пусть сортирует по алфавиту
+
+  // x < y  --> x.compareTo(y) < 0  --> x - y < 0
+  // x == y --> x.compareTo(y) == 0 --> x - y == 0
+  // x > y  --> x.compareTo(y) > 0  --> x - y > 0
+  // для чисел compareTo работает как "this - other"
+  // this < other
+  // this - other < 0
+
+  @Override
+  public int compareTo(Student other) {           //если рейтинг одинаковый
+    if (/*this.*/score != other.score) {          // то сравниваем рейтинг
+      return /*this.*/score - other.score;
+    }
+    return /*this.*/name.compareTo(other.name);
+  }
+}
