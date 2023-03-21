5f48e2b8660d905b11768ea3cf780bae4a9f6173
Leon Zuev <loonatune@gmail.com>
2023-03-21 21:52:47 +0200

    Add sort.

--- src/collections/StudentDatabase.java
+++ src/collections/StudentDatabase.java
@@ -15,6 +15,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StudentDatabase {
@@ -53,7 +54,7 @@ public class StudentDatabase {
     for (int i = 0; i < n; ++i) {
       students.add(Student.readStudent());
     }
-
+    Collections.sort(students);
     for (Student student : students) {
       System.out.println(student);
     }
